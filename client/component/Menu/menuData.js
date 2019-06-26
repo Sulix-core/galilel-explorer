80a9bcf2ef00a89dc31a31ed5a56ea57b46585af
Maik Broemme <mbroemme@libmpq.org>
2019-06-26 21:56:01 +0200

    Fixed missing API icon introduced with 1ee3dc9afbf39a238522a6477902e86390bd4e58

--- client/component/Menu/menuData.js
+++ client/component/Menu/menuData.js
@@ -7,7 +7,7 @@ const MenuData = [
   {label: 'Connections', icon: '/img/connections_mobile.svg', href: '/peer'},
   {label: 'Statistics', icon: '/img/statistics_mobile.svg', href: '/statistics'},
   {label: 'Coin Info', icon: '/img/coininfo_mobile.svg', href: '/coin'},
-  {label: 'API', icon: '/img/api.svg', href: '/api'}
+  {label: 'API', icon: '/img/api_mobile.svg', href: '/api'}
 ]
 
 export default MenuData;
